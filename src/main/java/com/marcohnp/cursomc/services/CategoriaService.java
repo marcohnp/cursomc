026def95830702456a27d945f545f07f59d0713d
Marco Henrique Nunes Pereira <marcoh@grupodimed.com.br>
2021-05-05 16:01:00 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/marcohnp/cursomc/services/CategoriaService.java
+++ src/main/java/com/marcohnp/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.marcohnp.cursomc.services;
 
 import com.marcohnp.cursomc.domain.Categoria;
 import com.marcohnp.cursomc.repositories.CategoriaRepository;
+import com.marcohnp.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class CategoriaService {
 
     public Categoria findById(Integer id){
         Optional<Categoria> objeto = repository.findById(id);
-        return objeto.orElse(null);
+        return objeto.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
