da265204a698fcc9fb91487c55411e9af18ce147
Marco Henrique Nunes Pereira <marcoh@grupodimed.com.br>
2021-05-11 10:20:29 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/marcohnp/cursomc/CursomcApplication.java
+++ src/main/java/com/marcohnp/cursomc/CursomcApplication.java
@@ -53,6 +53,11 @@ public class CursomcApplication  implements CommandLineRunner {
 
 		var cat1 = new Categoria(null, "Informática");
 		var cat2 = new Categoria(null, "Escritório");
+		var cat3 = new Categoria(null, "Cama mesa e banho");
+		var cat4 = new Categoria(null, "Eletrônicos");
+		var cat5 = new Categoria(null, "Jardinagem");
+		var cat6 = new Categoria(null, "Decoração");
+		var cat7 = new Categoria(null, "Perfumaria");
 
 		var p1 = new Produto(null, "Computador", 2000.00);
 		var p2 = new Produto(null, "Impressora", 800.00);
@@ -65,7 +70,7 @@ public class CursomcApplication  implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Collections.singletonList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		var est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/marcohnp/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcohnp/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import com.marcohnp.cursomc.domain.Categoria;
 import com.marcohnp.cursomc.dto.CategoriaDTO;
 import com.marcohnp.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -51,6 +52,16 @@ public class CategoriaResource {
     public ResponseEntity<Void> delete(@PathVariable Integer id){
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/page")
+    public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                       @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+                                                       @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+                                                       @RequestParam(value = "direction", defaultValue = "ASC") String direction){
+        Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listDTO = list.map(CategoriaDTO::new);
+        return ResponseEntity.ok().body(listDTO);
     }
 
 
