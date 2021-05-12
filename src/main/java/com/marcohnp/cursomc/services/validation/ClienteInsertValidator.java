d549e7a09f8e1abcd542cdb5fac52acc53277469
Marco Henrique Nunes Pereira <marcoh@grupodimed.com.br>
2021-05-12 15:50:46 -0300

    Validação customizada de CPF ou CNPJ na Inserção de CLiente

---
+++ src/main/java/com/marcohnp/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,40 @@
+package com.marcohnp.cursomc.services.validation;
+
+import com.marcohnp.cursomc.domain.enums.TipoCliente;
+import com.marcohnp.cursomc.dto.ClienteNewDTO;
+import com.marcohnp.cursomc.resources.exceptions.FieldMessage;
+import com.marcohnp.cursomc.services.validation.utils.BR;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod())
+                && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+        }
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod())
+                && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
