4634df46a44ed09416cecd4b8c40ea0097d28c45
Marco Henrique Nunes Pereira <marcoh@grupodimed.com.br>
2021-05-17 11:32:40 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/marcohnp/cursomc/resources/utils/URL.java
@@ -0,0 +1,24 @@
+package com.marcohnp.cursomc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class URL {
+
+    public static String decodeParam(String s) {
+       try{
+           return URLDecoder.decode(s, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+           return "";
+       }
+    }
+
+    public static List<Integer> decodeIntList(String s) {
+        return Arrays.stream(s.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+}
