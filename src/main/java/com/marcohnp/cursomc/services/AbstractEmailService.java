e65dad774fdd8427e00b78686d7786d3de45e216
Marco Henrique Nunes Pereira <marcoh@grupodimed.com.br>
2021-05-20 13:53:18 -0300

    Email HTML

--- src/main/java/com/marcohnp/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/marcohnp/cursomc/services/AbstractEmailService.java
@@ -1,15 +1,28 @@
 package com.marcohnp.cursomc.services;
 
 import com.marcohnp.cursomc.domain.Pedido;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Date;
 
 public abstract class AbstractEmailService implements EmailService {
 
     @Value("${default.sender}")
     private String sender;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     @Override
     public void sendOrderConfirmationEmail(Pedido pedido) {
@@ -25,5 +38,32 @@ public abstract class AbstractEmailService implements EmailService {
         sm.setSentDate(new Date(System.currentTimeMillis()));
         sm.setText(pedido.toString());
         return sm;
+    }
+
+    protected String htmlFromTemplatePedido(Pedido pedido) {
+        Context context = new Context();
+        context.setVariable("pedido", pedido);
+        return templateEngine.process("email/confirmacaoPedido", context);
+    }
+
+    @Override
+    public void sendOrderConfirmationHtmlEmail(Pedido pedido) {
+        try {
+            MimeMessage mm = prepareMimeMessageFromPedido(pedido);
+            sendHtmlEmail(mm);
+        } catch (MessagingException e) {
+            sendOrderConfirmationEmail(pedido);
+        }
+    }
+
+    protected MimeMessage prepareMimeMessageFromPedido(Pedido pedido) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+        mmh.setTo(pedido.getCliente().getEmail());
+        mmh.setFrom(sender);
+        mmh.setSubject("Pedido confirmado! Código: " + pedido.getId());
+        mmh.setSentDate(new Date(System.currentTimeMillis()));
+        mmh.setText(htmlFromTemplatePedido(pedido), true);
+        return mimeMessage;
     }
 }
